415f7499923c4663a5cb348636ec818814f410cb
joko-prasetyo <robertkwan.rk@gmail.com>
2019-11-01 05:36:01 +0700

    Third commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -102,6 +102,7 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
+        await user.save()
         res.send({ user, token })
     } catch (e) {
         res.status(400).send()
